fb72bf063b2eee40842d17c861ccb22486291166
yxfanSuda <111720091+yxfanSuda@users.noreply.github.com>
2025-03-10 20:42:12 +0800

    Update TST.sh

--- TST.sh
+++ TST.sh
@@ -9,6 +9,7 @@ train_ou10_file='./Data/Ou_Dataset/Ou10_Link_Dir/selectedData.json'
 train_ou15_file='./Data/Ou_Dataset/Ou15_Link_Dir/selectedData.json'
 dataset_dir="./selected_dataset_dir"
 model_dir="./model_dir"
+seed=65534 # Three independent runs with different random seeds (65534, 42, and 65535)are conducted and he mean performance is reported.
 
 if [ ! -d "${model_dir}" ]; then mkdir -p "${model_dir}"; fi
 
@@ -27,4 +28,4 @@ CUDA_VISIBLE_DEVICES=${GPU}  nohup python  -u main.py \
                                     --debug \
                                     --ST_model_path "${model_dir}/model_ST" \
                                     --TST_model_path "${model_dir}/model_TST" \
-                                    --seed 65534 > TST.log 2>&1 &
+                                    --seed seed > TST.log 2>&1 &
